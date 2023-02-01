5e5c5799dd6e00d1c1c6a123de5404360749d3ed
Unreal <t.babinos@gmail.com>
2023-02-01 16:17:46 +0200

    Initial Commit

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x6fba7194aa1048fe33fc52b5d1195e8365545707";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
